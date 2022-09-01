84192ba5c5711187921ea3f4c5c8c950d2da913e
Fabian Juarez <83832041+FabianJuarez182@users.noreply.github.com>
2022-08-31 20:34:40 -0600

    program completed with comments

--- Lab03-ejercicio02.cpp
+++ Lab03-ejercicio02.cpp
@@ -3,6 +3,11 @@
 * Laboratorio 3.cpp
 * -----------------------------------------
 * UNIVERSIDAD DEL VALLE DE GUATEMALA
+/*
+*------------------------------------------
+* Laboratorio 3.cpp
+* -----------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
 * FACULTAD DE INGENIERÍA
 * DEPARTAMENTO DE CIENCIA DE LA COMPUTACIÓN
 * CC3086 - Programacion de Microprocesadores
@@ -16,45 +21,44 @@
 #include <iostream>
 #include <pthread.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <unistd.h>
 #include <math.h>
 #include <sstream>
+
 using namespace std;
 pthread_mutex_t bloquear = PTHREAD_MUTEX_INITIALIZER;
 long exponente;
 double resultSUM;
 void *output;
+//Creacion de la serie geometrica con la cual se obtendra el valor de cada thread por separado
 void *serieGeometrica(void *arg){
     long exponente ;
     exponente = (long) arg;
     double numero = 2;
     double numerador = 3;
     double potencia;
-    //calcular el resultado de 2^n
-        potencia = pow(numero,exponente);
-
-    //calcular 3/2^n
+    
+    pthread_mutex_lock(&bloquear);//se bloqueara para que solo un thread acceda y permita ejecutarse de manera de espera
+    potencia = pow(numero,exponente);    //calcular el resultado de 2^n
+    //calculara el dato de 3/2^n y lo guardara en el resultado para ser enviado al total
     double* resultado = (double*) malloc(sizeof(double));
     *resultado =  (numerador/potencia) ;
-    pthread_mutex_lock(&bloquear);
     cout << "\n El valor de n: " << exponente << " resultado: " << *resultado << endl;
     pthread_mutex_unlock(&bloquear);
     return (void *) resultado;
 }
 int main() {
-    cout << "Ingresar el valor de n: " << endl;
+    cout << "Ingresar el valor de n: " << endl;//Ingreso de usuario
     cin >> exponente;
-    pthread_t threads[exponente+1];
+    pthread_t threads[exponente]; // cantidad de threads a implementar en un arrya de ellos
+    // Creacion y union de los threads para poderlos utilizar y encontrar el valor de la serie geometrica
     for (int i = 0; i <= exponente; i++) {
         pthread_create(&threads[i], NULL, serieGeometrica, (void *) i);
-    }
-    for(int i = 0; i <= exponente; i++){
         pthread_join(threads[i], &output);
         double result = *(double *) output;
         resultSUM += result;
     }
-    cout << "--------------------------------------------------------------" << endl;
+    cout << "\n--------------------------------------------------------------" << endl;
     cout << "\nresultado final posterior a la sumatoria de todos los resultados: " << resultSUM << endl;
     return 0;
 }
